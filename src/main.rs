f9518481845176aa3d220b3ed969ed73bf7b885a
Clément DOUIN <clement.douin@posteo.net>
2023-12-31 14:41:19 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ use keyring::Entry;
 
 #[tokio::main]
 async fn main() {
-    let passwd = Entry::new("a", "b").unwrap().get_password().unwrap();
+    let passwd = Entry::new("a").find_secret().await.unwrap();
     println!("passwd: {:?}", passwd);
 }
