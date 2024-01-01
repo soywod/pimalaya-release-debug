859db2535113f3ccb24bc42eab54ab082318d026
Clément DOUIN <clement.douin@posteo.net>
2024-01-01 10:33:31 +0100

    remove himalaya code

--- src/email/message/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod arg;
-pub mod attachment;
-pub mod command;
-pub mod config;
-pub mod template;
--- src/printer/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod print;
-pub mod print_table;
-pub mod printer;
-
-pub use print::*;
-pub use print_table::*;
-pub use printer::*;
