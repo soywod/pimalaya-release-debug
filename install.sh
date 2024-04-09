beb5ff37c9ade95fc509cb747b2399d040bc2908
Clément DOUIN <clement.douin@posteo.net>
2024-04-09 20:23:44 +0200

    fix macos ter

--- install.sh
+++ install.sh
@@ -9,25 +9,44 @@ die() {
 
 DESTDIR="${DESTDIR:-}"
 PREFIX="${PREFIX:-"$DESTDIR/usr/local"}"
-RELEASES_URL="https://github.com/soywod/neverest/releases"
+RELEASES_URL="https://github.com/soywod/pimalaya-release-debug/releases"
 
+binary=neverest
 system=$(uname -s | tr [:upper:] [:lower:])
+machine=$(uname -m | tr [:upper:] [:lower:])
+
 case $system in
-  msys*|mingw*|cygwin*|win*) system=windows; binary=neverest.exe ;;
-  linux|freebsd) system=linux; binary=neverest ;;
-  darwin) system=macos; binary=neverest ;;
-  *) die "Unsupported system: $system" ;;
+    msys*|mingw*|cygwin*|win*)
+	target=x86_64-pc-windows-gnu
+	binary=neverest.exe;;
+
+    linux|freebsd)
+	case $machine in
+	    x86_64) target=x86_64-unknown-linux-gnu;;
+	    arm64|aarch64) target=aarch64-unknown-linux-gnu;;
+	    *) die "Unsupported machine $machine for system $system";;
+	esac;;
+
+    darwin)
+	case $machine in
+	    x86_64) target=x86_64-apple-darwin;;
+	    arm64|aarch64) target=aarch64-apple-darwin;;
+	    *) die "Unsupported machine $machine for system $system";;
+	esac;;
+
+    *)
+	die "Unsupported system $system";;
 esac
 
-tmpdir=$(mktemp -d) || die "Failed to create tmpdir"
+tmpdir=$(mktemp -d) || die "Cannot create temporary directory"
 trap "rm -rf $tmpdir" EXIT
 
 echo "Downloading latest $system release…"
-curl -sLo "$tmpdir/neverest.tar.gz" \
-     "$RELEASES_URL/latest/download/neverest-$system.tar.gz"
+curl -sLo "$tmpdir/neverest.tgz" \
+     "$RELEASES_URL/latest/download/neverest.$target.tgz"
 
 echo "Installing binary…"
-tar -xzf "$tmpdir/neverest.tar.gz" -C "$tmpdir"
+tar -xzf "$tmpdir/neverest.tgz" -C "$tmpdir"
 
 mkdir -p "$PREFIX/bin"
 cp -f -- "$tmpdir/$binary" "$PREFIX/bin/$binary"
