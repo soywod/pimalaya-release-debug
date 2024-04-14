8ac11bd5a1de3497ec8b015fbe42df0e310a0d9b
Clément DOUIN <clement.douin@posteo.net>
2024-04-14 09:43:11 +0200

    try fix toolchain

--- rust-toolchain.nix
+++ rust-toolchain.nix
@@ -2,7 +2,7 @@ fenix:
 
 let
   file = ./rust-toolchain.toml;
-  sha256 = "3St/9/UKo/6lz2Kfq2VmlzHyufduALpiIKaaKX4Pq0g=";
+  sha256 = "+syqAd2kX8KVa8/U2gz3blIQTTsYYt3U63xBWaGOSc8=";
 in
 {
   fromFile = { system }: fenix.packages.${system}.fromToolchainFile {
@@ -12,16 +12,20 @@ in
   fromTarget = { pkgs, buildPlatform, targetPlatform ? null }:
     let
       inherit ((pkgs.lib.importTOML file).toolchain) channel;
-      toolchain = fenix.packages.${buildPlatform};
+      fenixToolchain = fenix.packages.${buildPlatform};
+      rustToolchain = fenix.packages.${buildPlatform}.fromToolchainName {
+        inherit sha256;
+        name = channel;
+      };
     in
     if
       isNull targetPlatform
     then
-      fenix.packages.${buildPlatform}.${channel}.toolchain
+      rustToolchain
     else
-      toolchain.combine [
-        toolchain.${channel}.rustc
-        toolchain.${channel}.cargo
-        toolchain.targets.${targetPlatform}.${channel}.rust-std
+      fenixToolchain.combine [
+        rustToolchain.rustc
+        rustToolchain.cargo
+        fenixToolchain.targets.${targetPlatform}.fromToolchainFile {inherit file sha256;}
       ];
 }
